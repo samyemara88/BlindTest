954d93786e8d311775c66ceb5d0386ed3dc7d952
Samy <Samy>
2017-05-04 18:25:59 +0200

    ddd

---
+++ MyBlindMusic/End.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyBlindMusic
+{
+    public partial class End : Form
+    {
+        public End()
+        {
+            InitializeComponent();
+        }
+
+        private void End_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
